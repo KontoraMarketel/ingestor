a3298f97726b89a3d87ae435ac6e0d157e6e8bb5
georgiygusev <georgiy.gus@yandex.ru>
2025-07-16 16:07:17 +0300

    added fbs orders

---
+++ fetchers/fbs_orders.py
@@ -0,0 +1,41 @@
+import json
+
+import requests
+
+
+def fetch_fbs_orders(api_token: str, yesterday: str):
+    headers = {
+        "Authorization": api_token
+    }
+    url = "https://marketplace-api.wildberries.ru/api/v3/orders"
+    limit = 1000
+
+    next_val = 0
+    all_orders = []
+
+    while True:
+        params = {
+            "limit": limit,
+            "next": next_val,
+            "dateFrom": yesterday,
+            "dateTo": yesterday
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        orders = data.get("orders", [])
+        all_orders.extend(orders)
+
+        next_val = data.get("next", 0)
+        if not orders or next_val == 0:
+            break
+
+    fbs_orders = [o for o in all_orders if o.get("deliveryType") == "fbs" and o.get("supplyId")]
+
+    # Формируем имя файла
+    filename = "fbs_orders.json"
+
+    raw_data_str = json.dumps(fbs_orders, indent=2)
+    return filename, raw_data_str
